c50019211c7bd543534edb8d6642363f7147abe2
Georg Marth <marth.georg@gmail.com>
2018-11-27 14:10:38 +0100

    Update PlayerCamera.cs

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -1,23 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerCamera : MonoBehaviour {
-
+public class PlayerCamera : MonoBehaviour
+{
     public Transform player;
-
     public Vector3 offset;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float cameraSpeed = .8f;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float lookAtSpeed = .8f;
-    
-	void FixedUpdate () {
+
+    void FixedUpdate()
+    {
         Quaternion playerRotation = Quaternion.Euler(new Vector3(0f, player.rotation.eulerAngles.y, 0f));
         Vector3 targetPos = player.position + (playerRotation * offset);
         transform.position = Vector3.Lerp(transform.position, targetPos, cameraSpeed);
 
         Quaternion targetRotation = Quaternion.LookRotation(player.position - transform.position, Vector3.up);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, lookAtSpeed);
-	}
+    }
 }
