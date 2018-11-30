9e3ac6c12104ff3fb3e48b55e617a4d0ba9cdb87
Georg Marth <marth.georg@gmail.com>
2018-11-30 13:38:53 +0100

    Health Script

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int amount;
+
+    public int StartHealth = 100;
+
+    /// <summary>
+    /// OnHealthChanged(int current, int delta)
+    /// delta is unclamped
+    /// </summary>
+    public Action<int, int> OnHealthChanged;
+
+    public Action OnDeath;
+
+    public int Amount => amount;
+
+    private void Start()
+    {
+        amount = StartHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        amount = Mathf.Max(0, amount - damage);
+        OnHealthChanged(amount, -damage);
+
+        if (amount == 0)
+            OnDeath();
+    }
+
+    public void GainHealth(int gain)
+    {
+        amount += gain;
+        OnHealthChanged(amount, gain);
+    }
+}
